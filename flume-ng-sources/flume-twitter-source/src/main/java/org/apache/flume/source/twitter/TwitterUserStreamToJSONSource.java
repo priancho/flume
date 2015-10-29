ea0848fe0b3997f018cd1523aa5f7359c92a924a
Han-Cheol Cho <hancheol.cho@nhn-playart.com>
2015-10-29 14:56:43 +0900

    1. Applied FLUME-2618 patch for using HTTP proxy. 2. Added a custome Twitter source, TwitterUserStreamToJSONSource, to collect Twitter's user stream (currently, only the onStatus event is targeted.)

--- flume-ng-sources/flume-twitter-source/src/main/java/org/apache/flume/source/twitter/TwitterUserStreamToJSONSource.java
+++ flume-ng-sources/flume-twitter-source/src/main/java/org/apache/flume/source/twitter/TwitterUserStreamToJSONSource.java
@@ -25,15 +25,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.nio.charset.Charset;
 
-import org.apache.avro.Schema;
-import org.apache.avro.Schema.Field;
-import org.apache.avro.Schema.Type;
-import org.apache.avro.file.DataFileWriter;
-import org.apache.avro.generic.GenericData.Record;
-import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumWriter;
 import org.apache.commons.lang.StringUtils;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
@@ -56,24 +49,29 @@ import twitter4j.TwitterStreamFactory;
 import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.UserStream;
+import twitter4j.UserList;
+import twitter4j.DirectMessage;
+import twitter4j.UserStreamListener;
+// Flume twitter4j library has DataObjectFactory, not TwitterObjectFactory
+import twitter4j.json.DataObjectFactory;
+
 
 /**
- * Demo Flume source that connects via Streaming API to the 1% sample twitter
- * firehose, continously downloads tweets, converts them to Avro format and
- * sends Avro events to a downstream Flume sink.
+ * A demo Flume source that connects to the UserStream Streaming API.
+ * Unlike TwitterSource, it downloads raw data in JSON and sends to a downstream
+ * Flume sink.
  *
- * Requires the consumer and access tokens and secrets of a Twitter developer
- * account
+ * Requires the consumer and access tokens and secrets of a Twitter developer account
  */
 
 @InterfaceAudience.Private
 @InterfaceStability.Unstable
-public class TwitterSource
+public class TwitterUserStreamToJSONSource
     extends AbstractSource
-    implements EventDrivenSource, Configurable, StatusListener {
+    implements EventDrivenSource, Configurable, UserStreamListener {
 
   private TwitterStream twitterStream;
-  private Schema avroSchema;
 
   private long docCount = 0;
   private long startTime = 0;
@@ -81,10 +79,10 @@ public class TwitterSource
   private long totalTextIndexed = 0;
   private long skippedDocs = 0;
   private long batchEndTime = 0;
-  private final List<Record> docs = new ArrayList<Record>();
+  private final List<String> docs = new ArrayList<String>();
   private final ByteArrayOutputStream serializationBuffer =
       new ByteArrayOutputStream();
-  private DataFileWriter<GenericRecord> dataFileWriter;
+  private final Charset charset = Charset.forName("UTF-8");
 
   private int maxBatchSize = 1000;
   private int maxBatchDurationMillis = 1000;
@@ -97,22 +95,22 @@ public class TwitterSource
   private static int REPORT_INTERVAL = 100;
   private static int STATS_INTERVAL = REPORT_INTERVAL * 10;
   private static final Logger LOGGER =
-      LoggerFactory.getLogger(TwitterSource.class);
+      LoggerFactory.getLogger(TwitterUserStreamToJSONSource.class);
 
-  public TwitterSource() {
+  public TwitterUserStreamToJSONSource() {
   }
 
   @Override
   public void configure(Context context) {
-    String consumerKey = context.getString("consumerKey");
-    String consumerSecret = context.getString("consumerSecret");
-    String accessToken = context.getString("accessToken");
+    String consumerKey       = context.getString("consumerKey");
+    String consumerSecret    = context.getString("consumerSecret");
+    String accessToken       = context.getString("accessToken");
     String accessTokenSecret = context.getString("accessTokenSecret");
-    String proxyHost = context.getString("http.proxyHost");
-    String proxyPort =        context.getString("http.proxyPort");
-    String proxyUser = context.getString("http.proxyUser");
-    String proxyPassword =        context.getString("http.proxyPassword");
-    ConfigurationBuilder cb = new ConfigurationBuilder();
+    String proxyHost     = context.getString("http.proxyHost");
+    String proxyPort     = context.getString("http.proxyPort");
+    String proxyUser     = context.getString("http.proxyUser");
+    String proxyPassword = context.getString("http.proxyPassword");
+    ConfigurationBuilder cb  = new ConfigurationBuilder();
     cb.setDebugEnabled(true);
     if (StringUtils.isNotEmpty(proxyHost))
       cb.setHttpProxyHost(proxyHost);
@@ -122,6 +120,7 @@ public class TwitterSource
       cb.setHttpProxyUser(proxyUser);
     if (StringUtils.isNotEmpty(proxyPassword))
       cb.setHttpProxyPassword(proxyPassword);
+    cb.setJSONStoreEnabled(true);
 
     LOGGER.info("Consumer Key:        '" + consumerKey + "'");
     LOGGER.info("Consumer Secret:     '" + consumerSecret + "'");
@@ -133,9 +132,6 @@ public class TwitterSource
     twitterStream.setOAuthAccessToken(new AccessToken(accessToken,
                                                       accessTokenSecret));
     twitterStream.addListener(this);
-    avroSchema = createAvroSchema();
-    dataFileWriter = new DataFileWriter<GenericRecord>(
-        new GenericDatumWriter<GenericRecord>(avroSchema));
 
     maxBatchSize = context.getInteger("maxBatchSize", maxBatchSize);
     maxBatchDurationMillis = context.getInteger("maxBatchDurationMillis",
@@ -151,7 +147,10 @@ public class TwitterSource
     totalTextIndexed = 0;
     skippedDocs = 0;
     batchEndTime = System.currentTimeMillis() + maxBatchDurationMillis;
-    twitterStream.sample();
+
+    // twitterStream.sample();
+    twitterStream.user();
+
     LOGGER.info("Twitter source {} started.", getName());
     // This should happen at the end of the start method, since this will 
     // change the lifecycle status of the component to tell the Flume 
@@ -170,17 +169,18 @@ public class TwitterSource
   }
 
   public void onStatus(Status status)  {
-    Record doc = extractRecord("", avroSchema, status);
-    if (doc == null) {
-      return; // skip
-    }
+    // create a new json with event type and data
+    String doc = "{" +
+      "\"eventType\":\"onStatus\", " +
+      "\"status\":" + DataObjectFactory.getRawJSON(status) + "}";
     docs.add(doc);
+    
     if (docs.size() >= maxBatchSize ||
         System.currentTimeMillis() >= batchEndTime) {
       batchEndTime = System.currentTimeMillis() + maxBatchDurationMillis;
       byte[] bytes;
       try {
-        bytes = serializeToAvro(avroSchema, docs);
+        bytes = serializeToJSON(docs);
       } catch (IOException e) {
         LOGGER.error("Exception while serializing tweet", e);
         return; //skip
@@ -189,6 +189,7 @@ public class TwitterSource
       getChannelProcessor().processEvent(event); // send event to the flume sink
       docs.clear();
     }
+    
     docCount++;
     if ((docCount % REPORT_INTERVAL) == 0) {
       LOGGER.info(String.format("Processed %s docs",
@@ -199,114 +200,13 @@ public class TwitterSource
     }
   }
 
-  private Schema createAvroSchema() {
-    Schema avroSchema = Schema.createRecord("Doc", "adoc", null, false);
-    List<Field> fields = new ArrayList<Field>();
-    fields.add(new Field("id", Schema.create(Type.STRING), null, null));
-    fields.add(new Field("user_friends_count",
-                         createOptional(Schema.create(Type.INT)),
-                         null, null));
-    fields.add(new Field("user_location",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("user_description",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("user_statuses_count",
-                         createOptional(Schema.create(Type.INT)),
-                         null, null));
-    fields.add(new Field("user_followers_count",
-                         createOptional(Schema.create(Type.INT)),
-                         null, null));
-    fields.add(new Field("user_name",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("user_screen_name",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("created_at",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("text",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("retweet_count",
-                         createOptional(Schema.create(Type.LONG)),
-                         null, null));
-    fields.add(new Field("retweeted",
-                         createOptional(Schema.create(Type.BOOLEAN)),
-                         null, null));
-    fields.add(new Field("in_reply_to_user_id",
-                         createOptional(Schema.create(Type.LONG)),
-                         null, null));
-    fields.add(new Field("source",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("in_reply_to_status_id",
-                         createOptional(Schema.create(Type.LONG)),
-                         null, null));
-    fields.add(new Field("media_url_https",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    fields.add(new Field("expanded_url",
-                         createOptional(Schema.create(Type.STRING)),
-                         null, null));
-    avroSchema.setFields(fields);
-    return avroSchema;
-  }
-
-  private Record extractRecord(String idPrefix, Schema avroSchema, Status status) {
-    User user = status.getUser();
-    Record doc = new Record(avroSchema);
-
-    doc.put("id", idPrefix + status.getId());
-    doc.put("created_at", formatterTo.format(status.getCreatedAt()));
-    doc.put("retweet_count", status.getRetweetCount());
-    doc.put("retweeted", status.isRetweet());
-    doc.put("in_reply_to_user_id", status.getInReplyToUserId());
-    doc.put("in_reply_to_status_id", status.getInReplyToStatusId());
-
-    addString(doc, "source", status.getSource());
-    addString(doc, "text", status.getText());
-
-    MediaEntity[] mediaEntities = status.getMediaEntities();
-    if (mediaEntities.length > 0) {
-      addString(doc, "media_url_https", mediaEntities[0].getMediaURLHttps());
-      addString(doc, "expanded_url", mediaEntities[0].getExpandedURL());
-    }
-
-    doc.put("user_friends_count", user.getFriendsCount());
-    doc.put("user_statuses_count", user.getStatusesCount());
-    doc.put("user_followers_count", user.getFollowersCount());
-    addString(doc, "user_location", user.getLocation());
-    addString(doc, "user_description", user.getDescription());
-    addString(doc, "user_screen_name", user.getScreenName());
-    addString(doc, "user_name", user.getName());
-    return doc;
-  }
-
-  private byte[] serializeToAvro(Schema avroSchema, List<Record> docList)
+  private byte[] serializeToJSON(List<String> docList)
       throws IOException {
     serializationBuffer.reset();
-    dataFileWriter.create(avroSchema, serializationBuffer);
-    for (Record doc2 : docList) {
-      dataFileWriter.append(doc2);
+    for (String doc2 : docList) {
+      serializationBuffer.write(doc2.getBytes(charset));
     }
-    dataFileWriter.close();
     return serializationBuffer.toByteArray();
-  }
-
-  private Schema createOptional(Schema schema) {
-    return Schema.createUnion(Arrays.asList(
-        new Schema[] { schema, Schema.create(Type.NULL) }));
-  }
-
-  private void addString(Record doc, String avroField, String val) {
-    if (val == null) {
-      return;
-    }
-    doc.put(avroField, val);
-    totalTextIndexed += val.length();
   }
 
   private void logStats() {
@@ -327,23 +227,70 @@ public class TwitterSource
                 numFormatter.format(exceptionCount)));
   }
 
+  // StatusListener abstract methods.
   public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
     // Do nothing...
   }
-
   public void onScrubGeo(long userId, long upToStatusId) {
     // Do nothing...
   }
-
   public void onStallWarning(StallWarning warning) {
     // Do nothing...
   }
-
   public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
     // Do nothing...
+  }
+
+  // UserStreamListener abstract methods.
+  public void onDeletionNotice(long directMessageId, long userId) {
+  }
+  public void onFriendList(long[] friendIds) {
+  }
+  public void onFavorite(User source, User target, Status favoritedStatus) {
+  }
+  public void onUnfavorite(User source, User target, Status unfavoritedStatus) {
+  }
+  public void onFollow(User source, User followedUser) {
+  }
+  public void onUnfollow(User source, User unfollowedUser) {
+  }
+  public void onDirectMessage(DirectMessage directMessage) {
+  }
+  public void onUserListMemberAddition(User addedMember, User listOwner, UserList list) {
+  }
+  public void onUserListMemberDeletion(User deletedMember, User listOwner, UserList list) {
+  }
+  public void onUserListSubscription(User subscriber, User listOwner, UserList list) {
+  }
+  public void onUserListUnsubscription(User subscriber, User listOwner, UserList list) {
+  }
+  public void onUserListCreation(User listOwner, UserList list) {
+  }
+  public void onUserListUpdate(User listOwner, UserList list) {
+  }
+  public void onUserListDeletion(User listOwner, UserList list) {
+  }
+  public void onUserProfileUpdate(User updatedUser) {
+  }
+  public void onUserSuspension(long suspendedUser) {
+  }
+  public void onUserDeletion(long deletedUser) {
+  }
+  public void onBlock(User source, User blockedUser) {
+  }
+  public void onUnblock(User source, User unblockedUser) {
+  }
+  public void onRetweetedRetweet(User source,User target, Status retweetedStatus) {
+  }
+  public void onFavoritedRetweet(User source,User target, Status favoritedRetweeet) {
+  }
+  public void onQuotedTweet(User source, User target, Status quotingTweet) {
   }
 
   public void onException(Exception e) {
     LOGGER.error("Exception while streaming tweets", e);
   }
 }
+
+
+
